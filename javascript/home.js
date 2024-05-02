4762cd1aa383a0d1694c47bc20e3f98cfe8ecb02
chamila04 <chamilasenaratne04@gmail.com>
2024-05-02 20:13:27 +0530

    booking modals

--- javascript/home.js
+++ javascript/home.js
@@ -1,3 +1,24 @@
-document.getElementById("signupbtn").addEventListener("click", function() {
-    // Navigate to the login page
-    window.location.href = "signup.html";})
+// signup button function
+$(document).ready(function(){
+    $("#signupbtn").on('click',function(){
+        window.open("signup.html", "_blank");
+    });
+});
+
+// booking modal functions
+$(document).ready(function(){
+    $(".checkdate").datepicker({
+        autoclose:true,
+        clearBtn:true,
+        endDate:'+30d',
+        startDate:'+1d',
+        format:'dd/mm/yyyy',
+        todayHighlight:true
+    });
+
+    $("#roomcount").bootstrapNumber({
+        center:true,
+        downClass:'danger',
+        upClass:'success'
+    });
+});
